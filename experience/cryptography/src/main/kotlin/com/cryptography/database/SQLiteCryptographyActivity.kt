506426def9f951998c098dd53ddf7dcb732dba85
pratyesh <pratyesh3@gmail.com>
2023-12-28 16:17:34 +0530

    minor changes

--- experience/cryptography/src/main/kotlin/com/cryptography/database/SQLiteCryptographyActivity.kt
+++ experience/cryptography/src/main/kotlin/com/cryptography/database/SQLiteCryptographyActivity.kt
@@ -3,9 +3,14 @@ package com.cryptography.database
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
@@ -14,6 +19,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
@@ -41,34 +47,56 @@ class SQLiteCryptographyActivity() : ComponentActivity() {
 
                 val scope = rememberCoroutineScope()
 
-                var personName by remember {
-                    mutableStateOf(Person())
+                var persons by remember {
+                    mutableStateOf<List<Person>?>(null)
                 }
 
                 LaunchedEffect(Unit) {
                     scope.launch(Dispatchers.IO) {
-                        personName = appDatabase.personDao().getAllPersons().first()
+                        persons = appDatabase.personDao().getAllPersons()
                     }
                 }
-                Text(text = " SQL Lite database cryptography Person is $personName")
+                Text(text = " SQL Lite database cryptography Person List ")
 
-                Button(onClick = {
-                    scope.launch(Dispatchers.IO) {
-                        appDatabase.personDao().insertPersons(
-                            generatePerson(
-                                id = 123
-                            )
-                        )
+                if (persons?.isNotEmpty() == true) {
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.spacedBy(
+                            space = 16.dp,
+                            alignment = Alignment.Top
+                        ),
+                        horizontalAlignment = Alignment.Start,
+                        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp)
+                    ) {
+                        itemsIndexed(
+                            items = persons!!,
+                            key = { _, person -> person.id }
+                        ) { _, person ->
+                            Text(text = "${person.firstName} ${person.lastName}")
+                            Text(text = "ID:${person.id} & CV-info ${person.cvInfo}")
+                        }
                     }
-                }) {
-                    Text(text = "Add New Person")
+                } else {
+                    Button(onClick = {
+                        scope.launch(Dispatchers.IO) {
+                            val personList = mutableListOf<Person>()
+                            for (i in 101..110) {
+                                personList.add(generatePerson(id = i.toLong()))
+                            }
+                            appDatabase.personDao().insertPersonList(personList)
+                        }
+                    }) {
+                        Text(text = "Add New List of Person")
+                    }
                 }
+
             }
         }
     }
 }
 
-fun generatePerson(id :Long): Person {
+
+fun generatePerson(id: Long): Person {
     return Person(
         id = id,
         firstName = "Abcd",
